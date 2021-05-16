bc974495628b26f1997c2d4f0c2a7db52f6613d9
Frost <magomedns@gmail.com>
2021-05-16 22:57:20 +0300

    in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,44 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+
+    extend: {
+
+      fontFamily: {
+      "open-sans": ["Open Sans", "Arial", "sans-serif"],
+      "tahoma": ["Tahoma", "Arial", "sans-serif"],
     },
-    variants: {
-        extend: {},
+      fontSize: {
+       '80px': '5.25rem',
+       '64px': '4rem',
+       '32px': '2rem',
+      },
+      screens: {
+        "3xl": "1790px",
+      },
+      backgroundColor: {
+      green: "#01753D",
+      gold: "#AE9B31",
+      dark: "#231F20",
     },
-    plugins: [],
+    backgroundImage: theme => ({
+         'tree-bg': "url('/assets/img/tree-bg.png')",
+        })
+
+    },
+    gridTemplateColumns: {
+
+         'header': '50px 100px 300px',
+         'footer': '200px minmax(900px, 1fr) 100px',
+
+        }
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
+
