a4a12fed8fedd7001701794a50bc23159e86d410
Frost <magomedns@gmail.com>
2021-05-21 17:48:06 +0300

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,35 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-
-
     extend: {
-
+      colors: {
+        'greenz': "#01753D",
+        'gold': "#AE9B31",
+        'dark': "#231F20",
+      },
       fontFamily: {
-      "open-sans": ["Open Sans", "Arial", "sans-serif"],
-      "tahoma": ["Tahoma", "Arial", "sans-serif"],
-    },
+        "open-sans": ["Open Sans", "Arial", "sans-serif"],
+        "tahoma": ["Tahoma", "Arial", "sans-serif"],
+      },
       fontSize: {
-       '80px': '5.25rem',
-       '64px': '4rem',
-       '32px': '2rem',
+        '80px': '5.25rem',
+        '64px': '4rem',
+        '32px': '2rem',
       },
       screens: {
         "3xl": "1790px",
       },
-      backgroundColor: {
-      green: "#01753D",
-      gold: "#AE9B31",
-      dark: "#231F20",
-    },
-    backgroundImage: theme => ({
-         'tree-bg': "url('/assets/img/tree-bg.png')",
-        })
+      backgroundImage: theme => ({
+        'tree-bg': "url('/assets/img/tree-bg.png')",
+      })
 
     },
     gridTemplateColumns: {
 
-         'header': '50px 100px 300px',
-         'footer': '200px minmax(900px, 1fr) 100px',
+      'header': '50px 100px 300px',
+      'footer': '200px minmax(900px, 1fr) 100px',
 
-        }
+    }
   },
 
   variants: {
